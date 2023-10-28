12c80e2790e12314831817dceaa7a3974168ed92
Irfan2309 <90820501+Irfan2309@users.noreply.github.com>
2023-10-28 20:42:09 +0400

    ui for fav and history

--- Coursework/url.cs
+++ Coursework/url.cs
@@ -22,49 +22,76 @@ namespace WebBrowser
         public string GetContent(string url)
         {
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            this.statusCode = (int)response.StatusCode;
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                this.statusCode = (int)response.StatusCode;
 
-            if (statusCode == 200)
-            {
-                return response.Content.ReadAsStringAsync().Result;
+                if (statusCode == 200)
+                {
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+                else if (statusCode == 400)
+                {
+                    return "400 Error! Bad Request";
+                }
+                else if (statusCode == 403)
+                {
+                    return "403 Error! Forbidden";
+                }
+                else if (statusCode == 404)
+                {
+                    return "404 Error! Not Found";
+                }
+                else
+                {
+                    return "HTTP Response Status Error Code " + statusCode;
+                }
             }
-            else if (statusCode == 400)
+            catch (HttpRequestException e)
             {
-                return "400 Error! Bad Request";
+                Console.WriteLine("ERROROROOOROOROROROOROROROROROOROROROR" + e.Message);
+                Console.WriteLine("ERROROROOOROOROROROOROROROROROOROROROR" + e.InnerException.Message);
+                return "HTTP Request Error: " + e.Message;
             }
-            else if (statusCode == 403)
+            catch (Exception e)
             {
-                return "403 Error! Forbidden";
+                Console.WriteLine("ERROROROOOROOROROROOROROROROROOROROROR" + e.Message);
+                Console.WriteLine("ERROROROOOROOROROROOROROROROROOROROROR" + e.InnerException.Message);
+                return "Error: " + e.Message;
             }
-            else if (statusCode == 404)
-            {
-                return "404 Error! Not Found";
-            }
-            else
-            {
-                return "HTTP Response Status Error Code " + statusCode;
-            }
+
         }
 
         // Function to retrieve the page title
         public string GetTitle(string content)
         {
-            string begin = "<title>";
-            string end = "</title>";
-
-            int start = content.IndexOf(begin, StringComparison.OrdinalIgnoreCase);
-            int stop = content.IndexOf(end, start, StringComparison.OrdinalIgnoreCase);
-
-            if (start != -1 && stop != -1)
+            try
             {
-                string title = content.Substring(start + begin.Length, stop - start - begin.Length);
-                return title.Trim();
+                string begin = "<title>";
+                string end = "</title>";
+
+                int start = content.IndexOf(begin, StringComparison.OrdinalIgnoreCase);
+                int stop = content.IndexOf(end, start, StringComparison.OrdinalIgnoreCase);
+
+                if (start != -1 && stop != -1)
+                {
+                    string title = content.Substring(start + begin.Length, stop - start - begin.Length);
+                    return title.Trim();
+                }
+                else
+                {
+                    return "Untitled Page";
+                }
             }
-            else
+            catch (Exception e)
             {
-                return "Untitled Page";
+
+                Console.WriteLine("ERROROROOOROOROROROOROROROROROOROROROR" + e.Message);
+                Console.WriteLine("ERROROROOOROOROROROOROROROROROOROROROR" + e.InnerException.Message);
+                return "Error: " + e.Message;
             }
+
         }
 
         //getter setter for page url
